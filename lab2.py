8d780d1f42ab61c114f51c74ce24d4eec621df4a
Zi Jian <yeezj.24@ichat.sp.edu.sg>
2025-05-08 14:03:54 +0800

    lab2.py modified

--- lab2.py
+++ lab2.py
@@ -16,19 +16,15 @@ def get_user_input() :
 
 def calc_average_temperature(temp_values) :
     average_temp = sum(temp_values) / len(temp_values)
-    print("Average temperature:" + str(average_temp))
     return average_temp
 
 def calc_min_max_temperature(temp_values) :
     min_temp = min(temp_values)
     max_temp = max(temp_values)
-    print("Minimum temperature:" + str(min_temp))
-    print("Maximum temperature:" + str(max_temp))
     return min_temp, max_temp
 
 def sort_temperatures(temp_values) :
     sorted_temp = sorted(temp_values)
-    print("Sorted temperatures:" + str(sorted_temp))
     return sorted_temp
 
 def calc_median_temperature(temp_values):
@@ -38,15 +34,19 @@ def calc_median_temperature(temp_values):
         median_temp = (sorted_temp[n//2 - 1] + sorted_temp[n//2]) / 2
     else:
         median_temp = sorted_temp[n//2]
-    print("Median temperature:" + str(median_temp))
     return median_temp
 
 def main():
       temp_values=get_user_input()
-      calc_average_temperature(temp_values)
-      calc_min_max_temperature(temp_values)
-      sort_temperatures(temp_values)
-      calc_median_temperature(temp_values)
+      avg_temp=calc_average_temperature(temp_values)
+      print("Average temperature:" + str(avg_temp))
+      min_temp,max_temp=calc_min_max_temperature(temp_values)
+      print("Minimum temperature:" + str(min_temp))
+      print("Maximum temperature:" + str(max_temp))
+      sorted_temp=sort_temperatures(temp_values)
+      print("Sorted temperatures:" + str(sorted_temp))
+      median_temp=calc_median_temperature(temp_values)
+      print("Median temperature:" + str(median_temp))
 
 if __name__ == "__main__": 
  main() 
