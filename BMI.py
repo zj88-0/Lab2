333eeab7e8ad7e80d3f3d26ee37716503ea536cf
Zi Jian <yeezj.24@ichat.sp.edu.sg>
2025-05-08 14:57:29 +0800

    bmi

--- BMI.py
+++ BMI.py
@@ -5,9 +5,12 @@ def calculate_bmi(height, weight):
  print("BMI = " + str(BMI))
  if BMI < 18.5: 
    print("Underweight")
+   return -1
  elif BMI >= 18.5 and BMI <= 25:
     print("Normal weight")
+    return 0
  elif BMI > 25:
     print("Overweight")
+    return 1
    
 calculate_bmi(weight=57, height=1.73)
